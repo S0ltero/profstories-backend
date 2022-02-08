73aa38ff4f8b7a7ba4ecf54133f8349c2175c440
S0ltero <AlexsSobko61@gmail.com>
2022-02-08 12:28:52 +0300

    Fix incorrect list of installed apps

--- config/settings.py
+++ config/settings.py
@@ -11,11 +11,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     # Third party apps
     "rest_framework",
-    "corsheaders",
-    # Local apps
-    "api",
+
 ]
 
 MIDDLEWARE = [
