9161e288d46c8233479c510f86977fea89389be7
S0ltero <AlexsSobko61@gmail.com>
2022-02-14 16:44:44 +0300

    Translate human-readable value to ru language

--- events/models.py
+++ events/models.py
@@ -4,6 +4,9 @@ from django.contrib.postgres.fields import ArrayField
 from account.models import Employer
 class Event(models.Model):
     class Modes(models.TextChoices):
+        ONLINE = "ONLINE", "Онлайн"
+        OFFLINE = "OFFLINE", "Оффлайн"
+        MIXED = "MIXED", "Смешанный"
 
     class Periodic(models.TextChoices):
         FIRST = "FIRST", "Впервые"
