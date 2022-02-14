c8f4add5f9c944eb38c54b4f17c214c01d7b7351
S0ltero <AlexsSobko61@gmail.com>
2022-02-14 13:18:05 +0300

    Add imports

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core import validators
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import AbstractUser
+
+from .managers import UserManager
 
 # Create your models here.
