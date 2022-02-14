91ef4f29ebb0450cf93eb519b2e9ea5e8b1ca402
S0ltero <AlexsSobko61@gmail.com>
2022-02-14 13:18:37 +0300

    Add custom `User` model

--- account/models.py
+++ account/models.py
@@ -5,4 +5,43 @@ from django.contrib.auth.models import AbstractUser
 
 from .managers import UserManager
 
-# Create your models here.
+class User(AbstractUser):
+    class Types(models.TextChoices):
+        PROFESSIONAL = "PROFESSIONAL", "Professional"
+        EMPLOYER = "EMPLOYER", "Employer"
+
+
+    CREATED = "CREATED"
+    MODERATION = "MODERATION"
+    VERIFIED = "VERIFIED"
+    VERIFICATION_LEVEL_CHOICES = [
+        (CREATED, "created"),
+        (MODERATION, "on_moderation"),
+        (VERIFIED, "verified")
+    ]
+
+    username = None
+    email = models.EmailField(
+        "Email адрес",
+        unique=True,
+        validators=[validators.validate_email],
+        error_messages={
+            "unique": "Пользователь с таким email уже существует.",
+        },
+    )
+    type = models.CharField(verbose_name="Тип", max_length=50, choices=Types.choices)
+
+    first_name = models.CharField(verbose_name="Имя", max_length=100)
+    last_name = models.CharField(verbose_name="Фамилия", max_length=100)
+    middle_name = models.CharField(verbose_name="Отчество", max_length=100, blank=True)
+    verification = models.CharField(
+        verbose_name="Уровень верификации",
+        max_length=50,
+        choices=VERIFICATION_LEVEL_CHOICES,
+        default=CREATED
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ()
+
+    objects = UserManager()
