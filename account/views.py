b5ac20a6dc98354a512a9cfe472e985e852503ca
S0ltero <AlexsSobko61@gmail.com>
2022-02-15 12:41:49 +0300

    Add imports

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
+from .models import Employer, Professional
+from .serializers import (EmployerSerialzier, ProfessionalSerialzier,
+                          EmployerCreateSerializer, ProfessionalCreateSerializer,
+                          EmployerDetailSerializer, ProfessionalDetailSerializer)
 # Create your views here.
