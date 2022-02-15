95638caffb094c9e005e13c2accc469339f1d580
S0ltero <AlexsSobko61@gmail.com>
2022-02-15 16:04:32 +0300

    Add `EventViewset`

--- events/views.py
+++ events/views.py
@@ -7,4 +7,64 @@ from rest_framework.pagination import PageNumberPagination
 from .models import Event
 from .serializers import EventSerialzier, EventDetailSerializer, EventCreateSerializer
 
-# Create your views here.
+
+class EventViewset(viewsets.GenericViewSet):
+    queryset = Event
+    serializer_class = EventSerialzier
+    pagination_class = PageNumberPagination
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return EventCreateSerializer
+        else:
+            return EventSerialzier
+
+    def retrieve(self, request, pk):
+        try:
+            event = self.queryset.objects.get(id=pk)
+        except Event.DoesNotExist:
+            return Response(f"Мероприятие {pk} не найдено", status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = self.serializer_class(event)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=False):
+            serializer.save(employer_id=request.user.id)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        pk = request.data["id"]
+        try:
+            event = self.queryset.objects.get(id=pk)
+        except Event.DoesNotExist:
+            return Response(f"Мероприятие {pk} не найдено", status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(event, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=False):
+            serializer.update(event, serializer.validated_data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request, *args, **kwargs):
+        try:
+            employers = self.queryset.objects.all()
+            page = self.paginate_queryset(employers)
+        except Event.DoesNotExist:
+            return Response("Мероприятия не найдены", status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = self.serializer_class(page, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    @action(detail=True, url_path='detail', url_name='detail', serializer_class=EventDetailSerializer)
+    def qdetail(self, request, pk=None):
+        try:
+            event = self.queryset.objects.get(id=pk)
+        except Event.DoesNotExist:
+            return Response(f"Мероприятие {pk} не найдено", status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = self.serializer_class(event)
+        return Response(serializer.data, status=status.HTTP_200_OK)
