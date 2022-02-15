904ad384f74e4ea3cfbda94b653e36bb2d27769a
S0ltero <AlexsSobko61@gmail.com>
2022-02-15 12:43:23 +0300

    Add `ProfessionalViewset`

--- account/views.py
+++ account/views.py
@@ -74,3 +74,67 @@ class EmployerViewset(viewsets.GenericViewSet):
         serializer = self.serializer_class(employer)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class ProfessionalViewset(viewsets.GenericViewSet):
+    queryset = Professional
+    serializer_class = ProfessionalSerialzier
+    pagination_class = PageNumberPagination
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return ProfessionalCreateSerializer
+        else:
+            return ProfessionalSerialzier
+
+    def retrieve(self, request, pk):
+        try:
+            professional = self.queryset.objects.get(user_id=pk)
+        except Employer.DoesNotExist:
+            return Response(f"Работодатель {pk} не найден", status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = self.serializer_class(professional)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data["user"] = request.user.id
+        serializer = self.get_serializer_class()
+        serializer = serializer(data=data)
+        if serializer.is_valid(raise_exception=False):
+            serializer.save(user_id=request.user.id)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        pk = request.data["id"]
+        try:
+            professional = self.queryset.objects.get(id=pk)
+        except Employer.DoesNotExist:
+            return Response(f"Работодатель {pk} не найден", status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(professional, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=False):
+            serializer.update(professional, serializer.validated_data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request, *args, **kwargs):
+        try:
+            professionals = self.queryset.objects.all()
+            page = self.paginate_queryset(professionals)
+        except Employer.DoesNotExist:
+            return Response("Работодатели не найдены", status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = self.serializer_class(page, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    @action(detail=True, url_path='detail', url_name='detail', serializer_class=ProfessionalDetailSerializer)
+    def qdetail(self, request, pk=None):
+        try:
+            professional = self.queryset.objects.get(user_id=pk)
+        except Employer.DoesNotExist:
+            return Response(f"Работодатель {pk} не найден", status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = self.serializer_class(professional)
+        return Response(serializer.data, status=status.HTTP_200_OK)
