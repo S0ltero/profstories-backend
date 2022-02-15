b73fa846d6c6133949e6b0b19bd2ab8ecd3cf551
S0ltero <AlexsSobko61@gmail.com>
2022-02-15 16:08:56 +0300

    Register `User` and `Professional` model in admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 from .models import *
+
+admin.site.register(User)
+admin.site.register(Professional)
+
 @admin.register(Employer)
 class AdminEmployer(admin.ModelAdmin):
     actions = None
