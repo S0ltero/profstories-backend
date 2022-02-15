5ccdcf8a51dd68a1a0c6ca16bdc55fe986c7a17e
S0ltero <AlexsSobko61@gmail.com>
2022-02-15 16:04:20 +0300

    Add imports to `event.views`

--- events/views.py
+++ events/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
+from .models import Event
+from .serializers import EventSerialzier, EventDetailSerializer, EventCreateSerializer
 
 # Create your views here.
