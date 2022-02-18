142d8b7defb5dc05b2c9ec23a048abcce8d4b374
S0ltero <AlexsSobko61@gmail.com>
2022-02-18 11:04:57 +0300

    Register `CreateCallbackView` in main `urls.py`

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from account.views import CallbackCreateView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("djoser.urls")),
     path("api/auth/", include("account.urls")),
     path("api/auth/", include("djoser.urls.authtoken")),
-    path("api/", include("events.urls"))
+    path("api/", include("events.urls")),
+    path("api/callback/", CallbackCreateView.as_view())
 ]
