6468ad86ddb0d9f7f00e90c090bfd18f2999f347
S0ltero <AlexsSobko61@gmail.com>
2022-02-24 20:16:09 +0300

    Register `NPOViewset` to `/non-profit` endpoint

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,7 @@ from .views import *
 router = DefaultRouter()
 router.register(r"employers", EmployerViewset, basename="employers")
 router.register(r"professionals", ProfessionalViewset, basename="professionals")
+router.register(r"non-profit", NPOViewset, basename="non-profit")
 
 urlpatterns = [
 ] + router.urls
