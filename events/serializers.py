d0bf847193ef1cd5f46aadbdddee9a55cdeb3b6a
S0ltero <AlexsSobko61@gmail.com>
2022-03-01 11:51:10 +0300

    Add to `Event` serializer `organizer` field

--- events/serializers.py
+++ events/serializers.py
@@ -1,13 +1,25 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Event
 
+User = get_user_model()
+
 
 class EventSerialzier(serializers.ModelSerializer):
-    
+    organizer = serializers.SerializerMethodField()
+
+    def get_organizer(self, obj):
+        if obj.user.type == User.Types.EMPLOYER:
+            organizer = obj.user.employer.company_name
+        elif obj.user.type == User.Types.COLLEGE:
+            organizer = obj.user.college.college_name
+
+        return organizer
+
     class Meta:
         model = Event
-        exclude = ("employer",)
+        fields = "__all__"
 
 
 class EventCreateSerializer(serializers.ModelSerializer):
@@ -18,6 +30,15 @@ class EventCreateSerializer(serializers.ModelSerializer):
 
 
 class EventDetailSerializer(serializers.ModelSerializer):
+    organizer = serializers.SerializerMethodField()
+
+    def get_organizer(self, obj):
+        if obj.user.type == User.Types.EMPLOYER:
+            organizer = obj.user.employer.company_name
+        elif obj.user.type == User.Types.COLLEGE:
+            organizer = obj.user.college.college_name
+
+        return organizer
 
     class Meta:
         model = Event
