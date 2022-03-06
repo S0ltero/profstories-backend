30c7e97bfd19bd8bc934ed362a3d9755c842b07d
S0ltero <AlexsSobko61@gmail.com>
2022-03-06 11:07:53 +0300

    Add `IsEmployer` permission

--- account/permissions.py
+++ account/permissions.py
@@ -2,3 +2,9 @@ from django.contrib.auth import get_user_model
 from rest_framework.permissions import BasePermission
 
 User = get_user_model()
+
+
+class IsEmployer(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        return user.type == User.Types.EMPLOYER
