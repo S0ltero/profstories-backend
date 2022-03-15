b4cacd82b7b4a88157b13914c61770a838ec1bad
S0ltero <AlexsSobko61@gmail.com>
2022-03-15 14:22:51 +0300

    Add `EmployerExport` proxy model

--- export/models.py
+++ export/models.py
@@ -13,3 +13,10 @@ class UserExport(User):
         verbose_name = User._meta.verbose_name
         verbose_name_plural = User._meta.verbose_name_plural
 
+
+class EmployerExport(Employer):
+    class Meta:
+        proxy = True
+        app_label = "export"
+        verbose_name = Employer._meta.verbose_name
+        verbose_name_plural = Employer._meta.verbose_name_plural
