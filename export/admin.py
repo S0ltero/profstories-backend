4ae10ef2ab130984a2960a61ea0e68a5445c0580
S0ltero <AlexsSobko61@gmail.com>
2022-03-15 14:23:46 +0300

    Import models to `export.admin`

--- export/admin.py
+++ export/admin.py
@@ -3,3 +3,9 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportActionModelAdmin
 
+from .models import (
+    EmployerExport,
+    ProfessionalExport,
+    TeacherExport,
+    UserExport
+)
