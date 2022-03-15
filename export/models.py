aa912229a359b58ee2ba4b8932ee70a0245ce660
S0ltero <AlexsSobko61@gmail.com>
2022-03-15 14:22:34 +0300

    Add `UserExport` proxy model

--- export/models.py
+++ export/models.py
@@ -4,3 +4,12 @@ from account.models import (
     Teacher,
     User
 )
+
+
+class UserExport(User):
+    class Meta:
+        proxy = True
+        app_label = "export"
+        verbose_name = User._meta.verbose_name
+        verbose_name_plural = User._meta.verbose_name_plural
+
