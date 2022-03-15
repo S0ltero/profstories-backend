2d37720464e9953f82f1e384881f693a701cd31f
S0ltero <AlexsSobko61@gmail.com>
2022-03-15 14:23:03 +0300

    Add `ProfessinalExport` proxy model

--- export/models.py
+++ export/models.py
@@ -20,3 +20,11 @@ class EmployerExport(Employer):
         app_label = "export"
         verbose_name = Employer._meta.verbose_name
         verbose_name_plural = Employer._meta.verbose_name_plural
+
+
+class ProfessionalExport(Professional):
+    class Meta:
+        proxy = True
+        app_label = "export"
+        verbose_name = Professional._meta.verbose_name
+        verbose_name_plural = Professional._meta.verbose_name_plural
