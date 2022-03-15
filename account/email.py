7885e793a67c0b64f3931fee3cb3b3f4577afb64
S0ltero <AlexsSobko61@gmail.com>
2022-03-15 14:20:02 +0300

    Add `TeacherRegisterEmail` message template

--- account/email.py
+++ account/email.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.conf import settings
+from templated_mail.mail import BaseEmailMessage
 
 from djoser import email
 
@@ -14,4 +15,12 @@ class PasswordResetEmail(email.PasswordResetEmail):
         context["uid"] = email.utils.encode_uid(user.pk)
         context["token"] = default_token_generator.make_token(user)
         context["url"] = settings.DJOSER["PASSWORD_RESET_CONFIRM_URL"].format(**context)
+        return context
+
+
+class TeacherRegisterEmail(BaseEmailMessage):
+    template_name = "email/teacher_register.html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
         return context
