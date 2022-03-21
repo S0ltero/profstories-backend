68c3a67c6519f02a9d7c2143ed9807dc458fc64f
S0ltero <AlexsSobko61@gmail.com>
2022-03-21 12:10:33 +0300

    Add `MissionQuestion` model

--- helper/models.py
+++ helper/models.py
@@ -8,3 +8,19 @@ class Mission(models.Model):
     class Meta:
         verbose_name = "Миссия"
         verbose_name_plural = "Миссии"
+
+
+class MissionQuestion(models.Model):
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE, related_name="questions")
+    question = models.TextField(verbose_name="Вопрос")
+    order = models.PositiveIntegerField(verbose_name="Номер вопроса")
+    answers = models.JSONField(verbose_name="Ответы")
+
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+        unique_together = ("mission", "order")
+
+    def __str__(self) -> str:
+        return self.question
+
