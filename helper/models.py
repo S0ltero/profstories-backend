ae578b01452c26659f84072e0e4eb522143c9b14
S0ltero <AlexsSobko61@gmail.com>
2022-03-21 12:10:12 +0300

    Add `Mission` model

--- helper/models.py
+++ helper/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Mission(models.Model):
+    coins = models.PositiveIntegerField(verbose_name="Монеты")
+    order = models.PositiveIntegerField(verbose_name="Номер миссии", unique=True)
+
+    class Meta:
+        verbose_name = "Миссия"
+        verbose_name_plural = "Миссии"
