4be52df9ca824acd85cd97f428f25486203d590b
S0ltero <AlexsSobko61@gmail.com>
2022-03-21 12:24:48 +0300

    Add `StudentMissionSerializer` serializer

--- helper/serializers.py
+++ helper/serializers.py
@@ -13,3 +13,17 @@ class SkillSerializer(serializers.ModelSerializer):
 
     def get_percent(self, obj):
         return 100 * (obj.points / 12)
+
+
+class StudentMissionSerializer(serializers.ModelSerializer):
+    questions_count = serializers.IntegerField(source="mission.questions.count")
+
+    class Meta:
+        model = StudentMission
+        exclude = ("student", "reaction", "answers")
+
+    def update(self, instance, validated_data):
+        instance.answers.update(validated_data.get("answers"))
+        instance.reaction = validated_data.get("reaction", instance.reaction)
+        instance.save()
+        return instance
