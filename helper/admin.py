8efe6bd8548d24cf5542d5166a5d356cf632f6e0
S0ltero <AlexsSobko61@gmail.com>
2022-03-21 15:33:37 +0300

    Add `StudentMissionInline` inline model

--- helper/admin.py
+++ helper/admin.py
@@ -5,3 +5,13 @@ from django_json_widget.widgets import JSONEditorWidget
 import nested_admin
 
 from .models import *
+
+
+class StudentMissionInline(nested_admin.NestedStackedInline):
+    model = StudentMission
+    extra = 0
+    classes = ["collapse"]
+
+    formfield_overrides = {
+        models.JSONField: {'widget': JSONEditorWidget},
+    }
