b5a7de44ffefd81a0ea8f776b34d19cdc712be62
S0ltero <AlexsSobko61@gmail.com>
2022-03-27 16:25:50 +0300

    Revert 0a0f9cf52e11405099b5b6f5bbc67d5bf7a98cc6

--- account/views.py
+++ account/views.py
@@ -527,7 +527,7 @@ class StudentViewset(viewsets.GenericViewSet):
 
     def get_permissions(self):
         if self.action == "create":
-            self.permission_classes = [AllowAny]
+            self.permission_classes = [IsAuthenticated, IsStudent]
         elif self.action == "update":
             self.permission_classes = [CurrentUserOrAdmin]
         else:
@@ -541,22 +541,19 @@ class StudentViewset(viewsets.GenericViewSet):
 
     def create(self, request, *args, **kwargs):
         data = request.data.copy()
-
-        user_serializer = CreateUserSerializer(data=data)
-        if not user_serializer.is_valid(raise_exception=False):
-            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        user_serializer.save()
-        data["user"] = user_serializer.data["id"]
+        data["user"] = request.user.id
 
         serializer = self.serializer_class(data=data)
-        if serializer.is_valid(raise_exception=False):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            user_serializer.instance.delete()
+        if not serializer.is_valid(raise_exception=False):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        user_serializer = UserSerializer(instance=request.user, data=data, partial=True)
+        if not user_serializer.is_valid(raise_exception=False):
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        user_serializer.update(request.user, user_serializer.validated_data)
+        serializer.save(user_id=request.user.id)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, pk=None):
         student = self.get_object()
