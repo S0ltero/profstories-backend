179683976ad469c6615c0156e7b7f574ba1e171e
S0ltero <AlexsSobko61@gmail.com>
2022-03-27 16:24:26 +0300

    Add `email` field to `StudentSerializer`

--- account/serializers.py
+++ account/serializers.py
@@ -220,6 +220,7 @@ class StudentProfessionalSerializer(serializers.ModelSerializer):
 
 
 class StudentSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(source="user.email", read_only=True)
     last_name = serializers.CharField(source="user.last_name", read_only=True)
     first_name = serializers.CharField(source="user.first_name", read_only=True)
     middle_name = serializers.CharField(source="user.middle_name", read_only=True)
