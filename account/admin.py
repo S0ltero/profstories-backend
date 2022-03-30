ef2b68d7c22dc5b1d6230611e2a9a1ff0fb693bc
S0ltero <AlexsSobko61@gmail.com>
2022-03-30 11:17:54 +0300

    Add `search_fields` to `AdminTeacher`

--- account/admin.py
+++ account/admin.py
@@ -170,6 +170,7 @@ class AdminTeacher(nested_admin.NestedModelAdmin):
     list_display = ("id", "email", "last_name", "first_name", "middle_name", "verification")
     list_display_links = ("id", "email")
     list_filter = ("verification",)
+    search_fields = ("email", "last_name", "first_name", "middle_name")
     exclude = ("password", "is_active", "is_staff", "is_superuser",
                "last_login", "date_joined", "user_permissions", "groups")
 
